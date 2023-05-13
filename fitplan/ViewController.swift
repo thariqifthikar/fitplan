023e734f4fe9419f52c37a214724b310f40ba6fe
Thariq Ifthikar <thariqifthikar@gmail.com>
2023-05-13 22:57:42 +0800

    Initial Commit

---
+++ fitplan/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  fitplan
+//
+//  Created by user230748 on 5/13/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
